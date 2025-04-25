09d3a49f65513295fad8d45637fd94ed1a314f5a
jksung <aajakobsung@gmail.com>
2025-04-25 11:01:36 +0900

    초기 커밋.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,394 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    public override void OnApplyTemplate()
+    {
+        base.OnApplyTemplate();
+
+        List<TestModel> list = new List<TestModel>();
+        for (int i = 1; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                list.Add(new() { GroupName = $"group_{i}", Name = $"name name name name {j}" });
+            }
+        }
+
+
+        ListCollectionView cv = new ListCollectionView(list);
+        cv.GroupDescriptions.Add(new PropertyGroupDescription("GroupName"));
+
+        listBox.ItemsSource = cv;
+    }
+
+    private void listBox_LayoutUpdated(object sender, EventArgs e)
+    {
+        ListBoxItem topListBoxItem1;
+        GroupItem topGroupItem1, topGroupItem2;
+        ContentPresenter topPresenter1, topPresenter2 = null;
+        double topOffset1, topOffset2 = -1;
+
+        // find the first ListBoxItem which is at the top of the control
+        topListBoxItem1 = this.GetItemAtMinimumYOffset<ListBoxItem>();
+        if (topListBoxItem1 == null)
+            return;
+
+        // get all group items order by their distance to the top
+        var groupItems = TreeHelper.FindVisualChildren<GroupItem>(this.listBox)
+                                   .OrderBy(this.GetYOffset)
+                                   .ToList();
+
+        // from the GroupItem, find the ContentPresenter on which we can apply the transform
+        topGroupItem1 = TreeHelper.FindVisualAncestor<GroupItem>(topListBoxItem1);
+
+        //topPresenter1 = TreeHelper.FindVisualChild<ContentPresenter>(topGroupItem1);
+        topPresenter1 = TreeHelper.FindChild<ContentPresenter>(topGroupItem1, "PART_Header");
+
+        if (topPresenter1 == null)
+        {
+
+            return;
+        }
+
+        topOffset1 = this.GetYOffset(topPresenter1);
+
+        // try to find the next GroupItem and its presenter
+        var index = groupItems.IndexOf(topGroupItem1);
+        if (index + 1 < groupItems.Count)
+        {
+            topGroupItem2 = groupItems.ElementAt(index + 1);
+            topPresenter2 = TreeHelper.FindVisualChild<ContentPresenter>(topGroupItem2);
+            topOffset2 = this.GetYOffset(topPresenter2);
+        }
+
+        // update transforms
+        if (topOffset2 < 0 || topOffset2 > topPresenter1.ActualHeight)
+            this.SetGroupItemOffset(topPresenter1, topOffset1);
+
+        if (topPresenter2 != null)
+            topPresenter2.RenderTransform = null;
+    }
+
+    private T GetItemAtMinimumYOffset<T>() where T : UIElement
+    {
+        var minOffset = double.MaxValue;
+        T topItem = null;
+        foreach (var item in TreeHelper.FindVisualChildren<T>(this.listBox))
+        {
+            var offset = this.GetYOffset(item);
+            if (Math.Abs(offset) <= Math.Abs(minOffset))
+            {
+                minOffset = offset;
+                topItem = item;
+            }
+        }
+
+        return topItem;
+    }
+
+    private void SetGroupItemOffset(ContentPresenter groupHeader, double offset)
+    {
+        if (groupHeader.RenderTransform as TranslateTransform == null)
+            groupHeader.RenderTransform = new TranslateTransform();
+
+        Panel.SetZIndex(groupHeader, 999);
+        ((TranslateTransform)groupHeader.RenderTransform).Y -= offset;
+    }
+
+    private double GetYOffset(UIElement uiElement)
+    {
+        var transform = (MatrixTransform)uiElement.TransformToVisual(this.listBox);
+        return transform.Matrix.OffsetY;
+    }
+}
+
+public class TestModel
+{
+    public string GroupName { get; set; }
+
+    public string Name { get; set; }
+}
+
+public class TreeHelper
+{
+    /// <summary>
+    /// Determiner whether an element is the logical ancestor of another item
+    /// </summary>
+    /// <param name="parent">Parent element</param>
+    /// <param name="source">Child element</param>
+    /// <returns>True if the Child element has the Parent element as its parent in the logical tree</returns>
+    public static bool IsLogicalAncestorOf(FrameworkElement parent, FrameworkElement source)
+    {
+        if (parent == source)
+        {
+            return true;
+        }
+
+        FrameworkElement current = source;
+        while (current != null && current.Parent != null)
+        {
+            if (current.Parent == parent)
+            {
+                return true;
+            }
+
+            current = current.Parent as FrameworkElement;
+        }
+
+        return false;
+    }
+
+    /// <summary>
+    /// Returns the first ancester of specified type
+    /// </summary>
+    public static T FindAncestor<T>(DependencyObject current)
+    where T : DependencyObject
+    {
+        current = VisualTreeHelper.GetParent(current);
+
+        while (current != null)
+        {
+            if (current is T)
+            {
+                return (T)current;
+            }
+            current = VisualTreeHelper.GetParent(current);
+        }
+        ;
+        return null;
+    }
+
+    /// <summary>
+    /// Returns a specific ancester of an object
+    /// </summary>
+    public static T FindAncestor<T>(DependencyObject current, T lookupItem)
+    where T : DependencyObject
+    {
+        while (current != null)
+        {
+            if (current is T && current == lookupItem)
+            {
+                return (T)current;
+            }
+            current = VisualTreeHelper.GetParent(current);
+        }
+        ;
+        return null;
+    }
+
+    /// <summary>
+    /// Finds an ancestor object by name and type
+    /// </summary>
+    public static T FindAncestor<T>(DependencyObject current, string parentName)
+    where T : DependencyObject
+    {
+        while (current != null)
+        {
+            if (!string.IsNullOrEmpty(parentName))
+            {
+                var frameworkElement = current as FrameworkElement;
+                if (current is T && frameworkElement != null && frameworkElement.Name == parentName)
+                {
+                    return (T)current;
+                }
+            }
+            else if (current is T)
+            {
+                return (T)current;
+            }
+            current = VisualTreeHelper.GetParent(current);
+        }
+        ;
+
+        return null;
+
+    }
+
+    /// <summary>
+    /// Looks for a child control within a parent by name
+    /// </summary>
+    public static T FindChild<T>(DependencyObject parent, string childName)
+    where T : DependencyObject
+    {
+        // Confirm parent and childName are valid.
+        if (parent == null) return null;
+
+        T foundChild = null;
+
+        int childrenCount = VisualTreeHelper.GetChildrenCount(parent);
+        for (int i = 0; i < childrenCount; i++)
+        {
+            var child = VisualTreeHelper.GetChild(parent, i);
+            // If the child is not of the request child type child
+            T childType = child as T;
+            if (childType == null)
+            {
+                // recursively drill down the tree
+                foundChild = FindChild<T>(child, childName);
+
+                // If the child is found, break so we do not overwrite the found child.
+                if (foundChild != null) break;
+            }
+            else if (!string.IsNullOrEmpty(childName))
+            {
+                var frameworkElement = child as FrameworkElement;
+                // If the child's name is set for search
+                if (frameworkElement != null && frameworkElement.Name == childName)
+                {
+                    // if the child's name is of the request name
+                    foundChild = (T)child;
+                    break;
+                }
+                else
+                {
+                    // recursively drill down the tree
+                    foundChild = FindChild<T>(child, childName);
+
+                    // If the child is found, break so we do not overwrite the found child.
+                    if (foundChild != null) break;
+                }
+            }
+            else
+            {
+                // child element found.
+                foundChild = (T)child;
+                break;
+            }
+        }
+
+        return foundChild;
+    }
+
+    /// <summary>
+    /// Looks for a child control within a parent by type
+    /// </summary>
+    public static T FindChild<T>(DependencyObject parent)
+        where T : DependencyObject
+    {
+        // Confirm parent is valid.
+        if (parent == null) return null;
+
+        T foundChild = null;
+
+        int childrenCount = VisualTreeHelper.GetChildrenCount(parent);
+        for (int i = 0; i < childrenCount; i++)
+        {
+            var child = VisualTreeHelper.GetChild(parent, i);
+            // If the child is not of the request child type child
+            T childType = child as T;
+            if (childType == null)
+            {
+                // recursively drill down the tree
+                foundChild = FindChild<T>(child);
+
+                // If the child is found, break so we do not overwrite the found child.
+                if (foundChild != null) break;
+            }
+            else
+            {
+                // child element found.
+                foundChild = (T)child;
+                break;
+            }
+        }
+        return foundChild;
+    }
+
+    /// <summary>
+    /// Find all visual children of a given type
+    /// </summary>
+    /// <typeparam name="T">Type of the children to find</typeparam>
+    /// <param name="obj">Source dependency object</param>
+    /// <returns>A list of T objects that are visual children of the source dependency object</returns>
+    public static List<T> FindVisualChildren<T>(DependencyObject obj) where T : DependencyObject
+    {
+        List<T> matches = new List<T>();
+        return FindVisualChildren(obj, matches);
+    }
+
+    /// <summary>
+    /// Find the first visual child of a given type
+    /// </summary>
+    /// <typeparam name="T">Type of the visual child to retrieve</typeparam>
+    /// <param name="obj">Source dependency object</param>
+    /// <returns>First child that matches the given type</returns>
+    public static T FindVisualChild<T>(DependencyObject obj) where T : DependencyObject
+    {
+        for (int i = 0; i < VisualTreeHelper.GetChildrenCount(obj); i++)
+        {
+            DependencyObject child = VisualTreeHelper.GetChild(obj, i);
+            if (child != null && child is T)
+            {
+                return (T)child;
+            }
+            else
+            {
+                T childOfchild = FindVisualChild<T>(child);
+                if (childOfchild != null)
+                {
+                    return childOfchild;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    /// <summary>
+    /// Find the first visual ancestor of a given type
+    /// </summary>
+    /// <typeparam name="T">Type of the ancestor</typeparam>
+    /// <param name="element">Source visual</param>
+    /// <returns>First ancestor that matches the type in the visual tree</returns>
+    public static T FindVisualAncestor<T>(UIElement element) where T : class
+    {
+        while (element != null && !(element is T))
+        {
+            element = (UIElement)VisualTreeHelper.GetParent(element);
+        }
+
+        return element as T;
+    }
+
+    /// <summary>
+    /// Find all visual children of a given type
+    /// </summary>
+    /// <typeparam name="T">Type of the children to find</typeparam>
+    /// <param name="obj">Source dependency object</param>
+    /// <param name="matches">List of matches</param>
+    /// <returns>A list of T objects that are visual children of the source dependency object</returns>
+    private static List<T> FindVisualChildren<T>(DependencyObject obj, List<T> matches) where T : DependencyObject
+    {
+        for (int i = 0; i < VisualTreeHelper.GetChildrenCount(obj); i++)
+        {
+            DependencyObject children = VisualTreeHelper.GetChild(obj, i);
+            if (children != null && children is T)
+            {
+                matches.Add((T)children);
+            }
+            FindVisualChildren(children, matches);
+        }
+
+        return matches;
+    }
+}
